de5f4f95c64ceda14bf2e48cbcfc0fa9466261dc
github5884 <github5884@example.com>
2018-09-30 12:09:27 +0900

    サーバアプリ * DBのINSERT/UPDATEクエリ実行処理を実装

--- src/server/sqlite_access.py
+++ src/server/sqlite_access.py
@@ -2,8 +2,7 @@
 
 import sqlite3
 
-dbpath = "db/lps.sqlite"
-
+# DBアクセスクラス
 class DbAccess:
 
     # コンストラクタ
@@ -11,7 +10,9 @@ class DbAccess:
         # DBファイルパスを取得
         self._dbpath = dbpath
     
-    # Selectクエリを実行
+    # SELECTクエリを実行
+    # @param select_query   SELECTクエリ
+    # @return               クエリ実行結果＠dict形式
     def exec_select_query(self, select_query):
         # DB接続
         db_conn = sqlite3.connect(self._dbpath)
@@ -29,13 +30,40 @@ class DbAccess:
             
             return_dicts.append(return_dict)
         
+        # DBから切断
+        db_conn.close()
+
         return return_dicts
+
+    # UPDATEクエリを実行
+    # @param update_query   UPDATEクエリ
+    def exec_update_query(self, update_query):
+        # DB接続
+        db_conn = sqlite3.connect(self._dbpath)
+        cursor = db_conn.cursor()
+
+        try:
+            # UPDATEクエリ実行
+            cursor.execute(update_query)
+        except sqlite3.Error as e:
+            print("sqlite3 access error : " + e.args[0])
+        
+        # コミット
+        db_conn.commit()
+
+        # DBから切断
+        db_conn.close()
 
 
 # メイン関数
 def main():
+    dbpath = "db/lps.sqlite"
+
     db_access = DbAccess(dbpath)
     print(db_access.exec_select_query("select * from category"))
+
+    #db_access.exec_update_query("insert into category(name) values('雑費')")
+    #db_access.exec_update_query("update category set name='その他' where id=5")
 
 
 # エントリポイント
